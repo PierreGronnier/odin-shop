3c55efa57e047d19fbd54b9c204b2d757fbf6dc4
Pierre Gronnier <pierre.gronnier@gmail.com>
2025-06-23 15:20:40 +0200

    init project and start routes

---
+++ src/routes.jsx
@@ -0,0 +1,24 @@
+import App from "./App.jsx";
+import { Home } from "./components/Home.jsx";
+import { Shop } from "./components/Shop.jsx";
+import { Error } from "./components/Error.jsx";
+
+export const routes = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      },
+    ],
+  },
+];
+
+export default routes;
